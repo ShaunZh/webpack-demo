f3add434df73d59b4d835e7345f400d91944ccc9
ShaunZh <308826842@qq.com>
2018-08-20 10:06:34 +0800

    2. loader-babel对ES6语法支持

--- app/sum.js
+++ app/sum.js
@@ -1,4 +1,4 @@
-module.exports = function(a, b) {
+module.exports = (a, b) => {
   return a + b;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -7,5 +7,10 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'build')
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, use: 'babel-loader', exclude: /node_modules/}
+    ]
   }
 }
